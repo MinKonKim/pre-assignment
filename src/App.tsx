6352455b47371c725364e0ac87504555ffe73dad
Minkon Kim <alsrhs98@gmail.com>
2024-12-07 12:47:53 +0900

    setting

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignUpPage";
+
+const App = () => (
+  <Router>
+    <Routes>
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/signup" element={<SignupPage />} />
+    </Routes>
+  </Router>
+);
+
+export default App;
---
+++ src/pages/SignupPage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SignupPage = () => {
+  return <div>u</div>;
+};
+
+export default SignupPage;
