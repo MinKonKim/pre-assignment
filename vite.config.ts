812b295c08fbb7a372a8d8d954cd97e85bf5c0ea
alsrhs98 <alsrhs98@gmail.com>
2024-12-08 21:42:46 +0900

    update : Sentry 적용

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), sentryVitePlugin({
+    org: "work-connect",
+    project: "javascript-react"
+  })],
+
   define: {
     "process.env": {},
   },
+
+  build: {
+    sourcemap: true
+  }
 });
