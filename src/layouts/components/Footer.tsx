9705842203ca1e8c100b4ee9487c71a18d78d00c
Minkon Kim <alsrhs98@gmail.com>
2025-01-19 22:35:15 +0900

    feat: 프로필 이미지 적용용

--- src/layouts/components/Footer.tsx
+++ src/layouts/components/Footer.tsx
@@ -1,21 +1,34 @@
 const Footer = () => {
   return (
-    <footer className="bg-gray-800 text-white p-4">
-      <div className="container mx-auto text-center">
-        <p>연락처: 010-2596-6958</p>
-        <p>이메일: alsrhs98@gmail.com</p>
-        <p>
-          깃헙:{" "}
+    <footer className="bg-gray-800 text-white py-4">
+      <div className="container mx-auto flex flex-col md:flex-row justify-between items-center px-4">
+        <div className="flex flex-col md:flex-row gap-2 md:gap-4 items-center">
           <a
-            href="https://github.com/MinKonKim"
+            href="https://github.com/MinKonKim/pre-assignment/blob/dev/README.md"
             target="_blank"
-            rel="noopener noreferrer"
-            className="text-blue-400"
+            className="hover:underline"
           >
-            https://github.com/MinKonKim
+            GitHub
           </a>
-        </p>
-        <p>&copy; 2025 MinkonKim. All rights reserved.</p>
+          <span className="hidden md:inline">|</span>
+          <a
+            href="mailto:alsrhs98@gmail.com"
+            className="hover:underline hidden md:inline"
+          >
+            alsrhs98@gmail.com
+          </a>
+          <span className="hidden md:inline">|</span>
+          <a
+            href="tel:+01025966958"
+            className="hover:underline hidden md:inline"
+          >
+            010-2596-6958
+          </a>
+        </div>
+
+        <div className="mt-2 md:mt-0 text-sm hidden md:block">
+          © 2025 Minkon Kim. All rights reserved.
+        </div>
       </div>
     </footer>
   );
