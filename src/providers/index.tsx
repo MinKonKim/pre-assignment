68d916e81fe91f2c27b5056147ee1220207c54fe
alsrhs98 <alsrhs98@gmail.com>
2025-01-20 11:34:26 +0900

    update

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -1,4 +1,5 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { ReactNode } from "react";
 
 const queryClient = new QueryClient();
@@ -9,7 +10,10 @@ interface ProviderProps {
 
 const Provider = ({ children }: ProviderProps) => {
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 };
 
